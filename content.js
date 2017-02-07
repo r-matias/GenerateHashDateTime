f637ffe59d5573d9130b20ccfa9ff1573b178f8e
Rafael Matias <rafael.matias@mstech.com.br>
2017-02-07 16:39:26 -0200

    first commit

---
+++ content.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var checkPageButton = document.getElementById('generateTimeStamp');
+  checkPageButton.addEventListener('click', function() {
+
+		var input = document.getElementById('inputTimeStamp');
+		input.value = new Date().YYYYMMDDHHMMSS();
+		input.select();
+		
+		document.execCommand("copy");
+		
+  }, false);
+}, false);
+
+
+Object.defineProperty(Date.prototype, 'YYYYMMDDHHMMSS', {
+    value: function() {
+        function pad2(n) {  // always returns a string
+            return (n < 10 ? '0' : '') + n;
+        }
+
+        return this.getFullYear() +
+               pad2(this.getMonth() + 1) + 
+               pad2(this.getDate()) +
+               pad2(this.getHours()) +
+               pad2(this.getMinutes()) +
+               pad2(this.getSeconds());
+    }
+});
